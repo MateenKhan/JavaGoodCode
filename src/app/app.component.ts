5ae8325fd96e3584cb133199a33c2983cc5d083a
MateenKhan <makjavaprogrammer@gmail.com>
2022-05-09 15:15:00 +0530

    6 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DataLoader } from './data-loader';
-import { Concept } from './models/concept';
 
 @Component({
   selector: 'app-root',
@@ -8,33 +7,7 @@ import { Concept } from './models/concept';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  concepts: Concept[] = [AppComponent.getDefaultConcept()];
-  currentConcept: Concept = AppComponent.getDefaultConcept();
+  constructor(public dataLoader: DataLoader) {}
 
-  ngOnInit() {
-    DataLoader.getConcepts().subscribe((data: Concept[]) => {
-      console.log(data);
-      this.concepts = data;
-      this.activateClass(this.concepts[0]);
-    });
-  }
-
-  static getDefaultConcept(): Concept {
-    return { name: '', active: false };
-  }
-
-  activateClass(concept: Concept) {
-    if (concept.active === true) {
-      return;
-    }
-    this.currentConcept = concept;
-    this.concepts.forEach(function (item) {
-      let isCurrentItem = item.name == concept.name;
-      if (isCurrentItem) {
-        item.active = true;
-      } else {
-        item.active = false;
-      }
-    });
-  }
+  ngOnInit() {}
 }
