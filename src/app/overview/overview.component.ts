16967db1c71ce20beef1a8f04bf83efb5167100b
MateenKhan <makjavaprogrammer@gmail.com>
2022-05-11 16:58:48 +0530

    4 files modified

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { DataLoader } from '../data-loader';
 import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css'],
 })
 export class OverviewComponent implements OnInit {
+  show: boolean = false;
+
   constructor(
     public dataLoader: DataLoader,
     private toastrService: ToastrService
   ) {}
   ngOnInit() {}
-  public showSuccess(): void {
-    this.toastrService.success('Message Success!', 'Title Success!');
+  toggle(): void {
+    this.show = !this.show;
   }
 }
