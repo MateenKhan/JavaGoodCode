aafd702c5eb9e91e6221ab38a26efb012061c5da
MateenKhan <makjavaprogrammer@gmail.com>
2022-05-11 16:15:41 +0530

    3 files modified

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,12 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataLoader } from '../data-loader';
-
+import { trigger, transition, animate, style } from '@angular/animations';
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css'],
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate(
+          '500ms ease-in',
+          style({ transform: 'translateX(0%)', opacity: 1 })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class OverviewComponent implements OnInit {
+  show: boolean = false;
+
   constructor(public dataLoader: DataLoader) {}
   ngOnInit() {}
+  toggle(): void {
+    this.show = !this.show;
+  }
 }
