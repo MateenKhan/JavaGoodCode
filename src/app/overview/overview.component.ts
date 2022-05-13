009f5064e2ac6659c7652ec96d5e7c7a776be4d0
Mateen <Mateen.khan91@t-mobile.com>
2022-05-13 05:31:46 +0530

    updated css

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -5,20 +5,20 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.css'],
+  styleUrls: ['./overview.component.scss'],
 })
 export class OverviewComponent implements OnInit {
   show: boolean = false;
 
   constructor(
     public dataLoader: DataLoader,
-    private toastrService: ToastrService
+    private toastService: ToastrService
   ) {}
   ngOnInit() {}
   toggle(): void {
     this.show = !this.show;
   }
   public showSuccess(): void {
-    this.toastrService.success('Copied!');
+    this.toastService.success('Copied!');
   }
 }
